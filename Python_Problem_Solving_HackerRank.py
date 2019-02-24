bb9f023f5e199bae2410ff95214adba4574e0814
HadeerSamir <32237553+HadeerSamir@users.noreply.github.com>
2019-02-24 16:51:19 +0200

    Update Python_Problem_Solving_HackerRank.py

--- Python_Problem_Solving_HackerRank.py
+++ Python_Problem_Solving_HackerRank.py
@@ -214,4 +214,24 @@ if __name__ == '__main__':
     for j in students:
         print(j)
 ###########################################################################################################################
+#Nested Lists          ******************CHECK***************************
+#############
+
+if __name__ == '__main__':
+    students = []
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+
+    scores = list(set(students[x][1] for x in range(len(students))))
+    scores.sort()
+
+    students = [x[0] for x in students if x[1] == scores[1]]
+    students.sort()
+
+    for j in students:
+        print(j)
+#############################################################################################################
 
