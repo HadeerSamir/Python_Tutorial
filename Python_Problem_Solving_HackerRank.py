9825f5c3fb0006ef0e248dae8b12b8ca59e2b4a1
HadeerSamir <32237553+HadeerSamir@users.noreply.github.com>
2019-02-24 11:46:30 +0200

    Update Python_Problem_Solving_HackerRank.py

--- Python_Problem_Solving_HackerRank.py
+++ Python_Problem_Solving_HackerRank.py
@@ -127,3 +127,26 @@ if __name__ == '__main__':
             break
 
 ###################################################################################################################
+
+# Finding the percentage 
+###########################
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    total_sum=0.0
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+
+    query_name = input()
+
+    for mark in student_marks[query_name]:
+        total_sum+=mark
+        
+
+    average = total_sum/3
+    
+    print("%.2f" % average)    # round to 2 decimal numbers
+   #######################################################################################################################
+
